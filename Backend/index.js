815f303afefd00003005dcd5b4859652ff094ff6
Tabishkhan18 <tabishkhan1811@gmail.com>
2025-03-03 12:46:34 +0530

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+
+
+
+const server = http.createServer(app);
+
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+
