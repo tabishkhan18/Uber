82b4c299a4fee0d0aa56eefcd589f95a529427cc
Tabishkhan18 <tabishkhan1811@gmail.com>
2025-04-17 16:56:20 +0530

    Final commit

--- Frontend/src/pages/Start.jsx
+++ Frontend/src/pages/Start.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { IoIosArrowRoundForward } from "react-icons/io";
 import { Link } from 'react-router-dom';
-
+import { HiMiniCodeBracket } from "react-icons/hi2";
 const Start = () => {
   return (
     <div>
-      <h1 className='hidden sm:flex justify-center items-center h-screen bg-black text-white text-2xl font-bold'>
+      <div className='hidden sm:flex justify-center flex-col items-center h-screen bg-black text-white text-2xl font-bold'>
         Use mobile phone to open the app
-      </h1>
+        <div className="fixed bottom-0 w-full flex justify-between md:p-10 p-5">
+          <a href='https://tabishkhan18.github.io/' className='text-neutral-400 md:text-md text-xs font-semibold flex gap-1 hover:text-white hover:underline transition-all duration-100 ease-in-out'>Contact developer @<HiMiniCodeBracket style={{ fontSize: '1rem', fontWeight: 'bold' }} />Tabish</a>
+          <h1 className='text-neutral-400 md:text-md text-xs font-semibold hover:text-white hover:underline transition-all duration-100 ease-in-out '><a href="mailto:tabishkhan1811@gmail.com" className='cursor-pointer'>tabishkhan1811@gmail.com</a></h1>
+        </div>
+
+      </div>
       <div className='sm:hidden bg-[url(/bg.jpg)] bg-cover flex flex-col h-lvh justify-between'>
         <div>
           <img className='w-40' src="/logo.webp" alt="" />
